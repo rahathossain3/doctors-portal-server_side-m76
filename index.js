072fc4eb9af6a5e7100702ebd2da1fc4f8ca2fc4
rahathossain3 <rahathossain210@gmail.com>
2022-05-15 03:16:53 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -13,7 +13,44 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://doctor_admin:<password>@cluster0.r6cxt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r6cxt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+//mongodb client
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        // console.log('database connected')
+        const serviceCollection = client.db('doctors_portal').collection('services');
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+
+            res.send(services)
+
+        })
+
+
+
+
+    }
+    finally {
+
+    }
+
+
+}
+
+run().catch(console.dir);
+
+
+
+
+// console.log(uri);
 
 app.get('/', (req, res) => {
     res.send('Hello form doctor Uncle!')
